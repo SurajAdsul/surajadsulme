d9da1022d740a4ae438aa4247725d1d658c29b63
Suraj Adsul <surajcafe2@gmail.com>
2020-05-17 12:09:09 +0530

    Fixed font for page

--- resources/views/projects/details.blade.php
+++ resources/views/projects/details.blade.php
@@ -5,8 +5,9 @@
     <div class="lg:pl-32 mt-12">
         <div class="max-w-2xl">
             <h1 class="text-2xl font-extrabold text-black mb-4">{{ $page->title }}</h1>
-            <div class="text-lg text-grey-100 leading-normal spaced-y-6">
-                <p class="text-grey-100">
+            <div class="blog-post-color text-base leading-normal spaced-y-6">
+{{--                <p class="text-grey-100">--}}
+                <p class="text-base leading-normal mt-1">
                     {!! $page->body !!}
                 </p>
             </div>
